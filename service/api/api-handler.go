7151c49cb0e97c664f3a5076be82464e44051c91
Davide Stumm <stumm.2183473@uniroma1.it>
2024-12-09 18:13:25 +0100

    Add initial files for API endpoints as outlined in the documentation

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -10,6 +10,33 @@ func (rt *_router) Handler() http.Handler {
 	rt.router.GET("/", rt.getHelloWorld)
 	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
+	// Login
+	rt.router.POST("/session", rt.login)
+
+	// Search
+	rt.router.GET("/users", rt.searchUsers)
+
+	// User Profile
+	rt.router.PUT("/user-profile", rt.changeUsername)
+	rt.router.GET("/user-profile", rt.listConversations)
+	rt.router.PUT("/profile-picture", rt.changeProfilePicture)
+
+	// Conversation
+	rt.router.GET("/conversations/:partner-username", rt.showConversation)
+	rt.router.POST("/conversations/:partner-username", rt.startNewConversation)
+	rt.router.POST("/conversations/:partner-username/messages/:message-timestamp", rt.forwardMessage)
+	rt.router.DELETE("/conversations/:partner-username/messages/:message-timestamp", rt.deleteMessage)
+
+	// Comment
+	rt.router.PUT("/conversations/:partner-username/messages/:message-timestamp/comment", rt.makeComment)
+	rt.router.DELETE("/conversations/:partner-username/messages/:message-timestamp/comment", rt.deleteComment)
+
+	// Groups
+	rt.router.PUT("/groups", rt.addToGroup)
+	rt.router.PUT("/groups/{groupname}", rt.changeGroupName)
+	rt.router.DELETE("/groups/:groupname", rt.leaveGroup)
+	rt.router.PUT("/groups/:groupname/group-photo", rt.changeGroupPicture)
+
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
 
