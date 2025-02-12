c02fd363c35c8604c01c28b4843704e9626bc084
Davide Stumm <stumm.2183473@uniroma1.it>
2025-02-12 21:36:30 +0100

    Moved Dockerfile.frontend to main root and adjusted it so it still finds package.js. Removed Home- and SignUp-View.

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
 import ProfileView from "@/views/ProfileView.vue";
 import GroupSettingsView from "@/views/GroupSettingsView.vue";
 import LoginView from "@/views/LoginView.vue"; 
@@ -8,13 +7,11 @@ import ChatView from "@/views/ChatView.vue";
 import SearchView from "@/views/SearchView.vue";
 import ChoosingGroupMembersView from "@/views/ChoosingGroupMembersView.vue";
 import SetupNewGroupView from "@/views/SetupNewGroupView.vue";
-import SignUpView from "@/views/SignUpView.vue";
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", component: HomeView }, // Start-Site, decide Login or Sign Up
-    { path: "/login", component: LoginView }, // Login-Site
+    { path: "/", component: LoginView }, // Login-Site
     { path: "/chats", component: ChatListView }, // Chatlist after Login
     { path: "/profile", component: ProfileView }, // Profile Settings-Site
     { path: "/group-settings", component: GroupSettingsView }, // Group Settings-Site
@@ -22,7 +19,6 @@ const router = createRouter({
     { path: "/search", component: SearchView }, // Search Users-Site
     { path: "/choose-members", component: ChoosingGroupMembersView }, // Choosing Group Members-Site
     { path: "/create-group", component: SetupNewGroupView }, // Group Setup-Site
-    { path: "/sign-up", component: SignUpView }, // Sign Up-Site
   ],
 });
 
